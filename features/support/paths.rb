305a3f4f1c1c72418b4d17bbcf0b88d2b9f940da
Fiona <fbattersby7@gmail.com>
2016-11-08 09:20:45 +0000

    Redo of adding everything to features with capybara and cucumber

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,9 +15,6 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/'
-      
-    when /create new page/
-    '/movies/new'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
