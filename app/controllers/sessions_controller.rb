58b5cb97fbd107bce146ebe1dc3f7e4dc09edc15
Xaviera <a92347@aup.edu>
2016-12-12 18:54:34 +0000

    logging in now works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:user][:email])
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    if user && user.password==(params[:user][:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
+      session[:isProfessor] = user.isProfessor
+      flash[:notice]= "Welcome, "+user.name
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
-      flash[:notice] = "Password or email incorrect."
+      flash[:notice] = "Password or email incorrect. and it should be "+user.password
       redirect_to '/login'
     end
   end
