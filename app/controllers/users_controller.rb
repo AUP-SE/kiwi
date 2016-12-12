b3de61fa55202e36209bae57993abaac4bc98a16
Xaviera <a92347@aup.edu>
2016-12-12 13:22:38 +0000

    SIGNUP WORKS AND IT IS GLORIOUS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,26 @@ class UsersController < ApplicationController
     def show
     end
     
+    def create 
+        user = User.new(user_params)
+        if !(user.password=="" || user.passwordConfirm=="") && (user.password != user.passwordConfirm)
+         flash[:notice] = "Passwords do not match "
+         redirect_to '/signup'
+        
+        elsif user.save
+            session[:user_id] = user.id
+            
+            flash[:notice] = "Welcome #{user.name}!"
+            redirect_to '/'
+        else
+            flash[:warning] = "Please fill in all fields."
+            redirect_to '/signup'
+        end
+    end
+        
+     
     
-    def create
+    def create1notToUSe
      if params[:password]!=params[:passwordConfirm]
          flash[:notice] = "Passwords do not match; "+params[:password]+" "+params[:passwordConfirm]
          redirect_to '/signup'
